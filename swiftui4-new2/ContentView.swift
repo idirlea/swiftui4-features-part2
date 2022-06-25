d8435996af28c6dbfeaa20f8fbb3f75cc9cf8204
Ionut Dirlea <ionut.dirlea@gmail.com>
2022-06-25 12:49:34 +0300

    Initial Commit

---
+++ swiftui4-new2/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  swiftui4-new2
+//
+//  Created by Ionut Dirlea on 25.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui4-new2/swiftui4_new2App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui4_new2App.swift
+//  swiftui4-new2
+//
+//  Created by Ionut Dirlea on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui4_new2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
